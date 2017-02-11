08446bd975a22498096b7e7c61f40ffd72f86d6a
Andrew Monks <a@monks.co>
2017-02-11 14:45:37 -0600

    use `exec` not `match`

--- src/tag.js
+++ src/tag.js
@@ -1,7 +1,7 @@
 import $ from 'shelljs'
 
 const getRemote = () => $.exec('git remote -v').stdout
-const matchRemote = str => /github\.com.([a-zA-Z0-9-]*\/[a-zA-Z0-9-]*)/.match(str)[1]
+const matchRemote = str => /github\.com.([a-zA-Z0-9-]*\/[a-zA-Z0-9-]*)/.exec(str)[1]
 const remote = () => matchRemote(getRemote())
 
 const isClean = () => $.exec('git status --porcelain').stdout.trim() === ''
