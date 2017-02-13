9f96cb5f950bf897f29c98db68d122ea611464ca
Andrew Monks <a@monks.co>
2017-02-13 01:32:47 -0600

    feat: remote supports repos with multiple origins

--- src/remote.js
+++ src/remote.js
@@ -1,7 +1,10 @@
 import $ from 'shelljs'
 
 const getRemote = () => $.exec('git remote -v').stdout
-const matchRemote = str => /github\.com.([a-zA-Z0-9_-]*\/[a-zA-Z0-9_-]*)/.exec(str)[1]
+
+const matchRemote = (str, remote = '') =>
+  (new RegExp(`^${remote}.+?github\.com.([a-zA-Z0-9_-]*\/[a-zA-Z0-9_-]*)`, 'm')).exec(str)[1]
+
 const remote = () => matchRemote(getRemote())
 
 export default remote
