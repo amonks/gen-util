cb205946d12767bee9fdde95c78e35069abf6a76
Andrew Monks <a@monks.co>
2017-02-13 01:33:13 -0600

    chore: makeRepo uses remote

--- src/makeRepo.js
+++ src/makeRepo.js
@@ -1,15 +1,12 @@
 import $ from 'shelljs'
 import ask from './ask'
+import remote from './remote'
 
 const makeRepo = async () => {
-  const matchRepo = /[a-zA-Z0-9_-]*\/[a-zA-Z0-9_-]*/
   if ($.which('hub')) {
     const hubArg = await ask('project repo? (arg to `hub create`)')
     await $.exec(`hub create ${hubArg}`)
-    let matches = hubArg.match(matchRepo)
-    if (matches) {
-      return `https://github.com/${matches[0]}.git`
-    }
+    return `https://github.com/${remote()}.git`
   }
   const repo = await ask('project clone url?')
   return repo
