d7ad6fb0a6985f69bf60456105d06a9c2f60ee77
Andrew Monks <a@monks.co>
2017-02-13 00:07:48 -0600

    fix: deprecate tag in favor of getRepo
    does the same thing, better name. Will be removed in the next major.

--- src/getCommit.js
+++ src/getCommit.js
@@ -1,17 +1,15 @@
 import $ from 'shelljs'
 
-const getRemote = () => $.exec('git remote -v').stdout
-const matchRemote = str => /github\.com.([a-zA-Z0-9-]*\/[a-zA-Z0-9-]*)/.exec(str)[1]
-const remote = () => matchRemote(getRemote())
+import remote from './remote'
 
 const isClean = () => $.exec('git status --porcelain').stdout.trim() === ''
 const sha = () => $.exec('git rev-parse HEAD').stdout.trim()
 
-const tag = async () => {
+const origin = async () => {
   if (!isClean()) throw Error('Unclean!')
   const tag = `${remote()}@${sha()}`
   return tag
 }
 
-export default tag
+export default origin
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 export ask from './ask'
 export confirm from './confirm'
 export replacer from './replacer'
-export tag from './tag'
 export remote from './remote'
+
+import getCommit from './getCommit'
+export const tag = async () => {
+  console.warn('tag is deprecated. use "getCommit"')
+  return await getCommit()
+}
 
